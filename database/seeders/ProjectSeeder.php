6855e4bd309e23750ed29a4037bb3a70f4812c30
GabrieleLombard01 <gabriellombardo2001@gmail.com>
2023-08-31 17:54:13 +0200

    fakers data add

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Project;
+use Faker\Generator;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -10,8 +12,17 @@ class ProjectSeeder extends Seeder
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run(Generator $faker): void
     {
-        //
+        for ($i = 1; $i <= 5; $i++) {
+            $project = new Project();
+            $project->title = $faker->text(50);
+            $project->description = $faker->paragraphs(20, true);
+            $project->slug = str_replace(' ', '-', strtolower($project->title));
+            $project->thumb = $faker->imageUrl(250, 250);
+            $project->category = $faker->text(100);
+            $project->status = $faker->text(50);
+            $project->save();
+        }
     }
 }
