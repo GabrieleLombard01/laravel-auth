d9483760a9a7034e283d7d731e48dc9c12002c92
GabrieleLombard01 <gabriellombardo2001@gmail.com>
2023-08-31 19:21:04 +0200

    Routes for crud add

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Guest\HomeController as GuestHomeController;
 use App\Http\Controllers\Admin\HomeController as AdminHomeController;
+use App\Http\Controllers\Admin\ProjectController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -21,6 +22,14 @@ Route::get('/', [GuestHomeController::class, 'index'])->name('guest.home');
 Route::prefix('/admin')->name('admin.')->middleware(['auth'])->group(function () {
     Route::get('/', [AdminHomeController::class, 'index'])->name('home');
 });
+
+Route::get('/admin/projects', [ProjectController::class, 'index'])->name('admin.projects.index');
+Route::get('/admin/projects/create', [ProjectController::class, 'create'])->name('admin.projects.create');
+Route::get('/admin/projects/{project}', [ProjectController::class, 'show'])->name('admin.projects.show');
+Route::get('/admin/projects/{project}/edit', [ProjectController::class, 'edit'])->name('admin.projects.edit');
+Route::put('/admin/projects/{project}/update', [ProjectController::class, 'update'])->name('admin.projects.update');
+Route::post('/admin/projects/store', [ProjectController::class, 'store'])->name('admin.projects.store');
+Route::delete('/admin/projects/{project}', [ProjectController::class, 'destroy'])->name('admin.projects.destroy');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
