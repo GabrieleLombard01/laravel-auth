45d0099c33442cad1524808c60adc7c1305fad5f
GabrieleLombard01 <gabriellombardo2001@gmail.com>
2023-08-31 19:38:13 +0200

    index function fix

--- routes/web.php
+++ routes/web.php
@@ -20,16 +20,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [GuestHomeController::class, 'index'])->name('guest.home');
 
 Route::prefix('/admin')->name('admin.')->middleware(['auth'])->group(function () {
+    // HOME
     Route::get('/', [AdminHomeController::class, 'index'])->name('home');
+    // PROJECTS
+    Route::resource('projects', ProjectController::class);
 });
 
-Route::get('/admin/projects', [ProjectController::class, 'index'])->name('admin.projects.index');
-Route::get('/admin/projects/create', [ProjectController::class, 'create'])->name('admin.projects.create');
-Route::get('/admin/projects/{project}', [ProjectController::class, 'show'])->name('admin.projects.show');
-Route::get('/admin/projects/{project}/edit', [ProjectController::class, 'edit'])->name('admin.projects.edit');
-Route::put('/admin/projects/{project}/update', [ProjectController::class, 'update'])->name('admin.projects.update');
-Route::post('/admin/projects/store', [ProjectController::class, 'store'])->name('admin.projects.store');
-Route::delete('/admin/projects/{project}', [ProjectController::class, 'destroy'])->name('admin.projects.destroy');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
