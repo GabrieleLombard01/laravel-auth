82e697e7bae8f73b5d2391337b4545011c32b1dd
GabrieleLombard01 <gabriellombardo2001@gmail.com>
2023-09-01 01:24:02 +0200

    nav style fix

--- resources/views/includes/layout/navbar.blade.php
+++ resources/views/includes/layout/navbar.blade.php
@@ -1,28 +1,28 @@
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+        <nav class="navbar navbar-expand-md navbar-light nav_box shadow-sm">
             <div class="container">
                 <a class="navbar-brand d-flex align-items-center" href="{{ url('/') }}">
                     <div class="logo_laravel">
-                        Auth
+                        <img width="50px" src="{{ asset('images/code.png') }}" alt="logo">
                     </div>
-                    {{ config('app.name', 'Laravel') }}
+                    {{-- config('app.name', 'Laravel') --}}
                 </a>
 
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-                    aria-label="{{ __('Toggle navigation') }}">
+                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                    aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
 
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav me-auto">
+                    <ul class="navbar-nav me-auto ">
                         <li class="nav-item">
-                            <a class="nav-link @if (request()->routeIs('admin.home')) active @endif"
+                            <a class="nav-link fw-bolder text-white @if (request()->routeIs('admin.home')) active @endif"
                                 href="{{ url('/') }}">{{ __('Home') }}</a>
                         </li>
                         @auth
                             <li class="nav-item">
-                                <a class="nav-link @if (request()->routeIs('admin.projects*')) active @endif"
+                                <a class="nav-link fw-bolder text-white @if (request()->routeIs('admin.projects*')) active @endif"
                                     href="{{ route('admin.projects.index') }}">Progetti</a>
                             </li>
                         @endauth
@@ -34,24 +34,27 @@
                         <!-- Authentication Links -->
                         @guest
                             <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                                <a class="nav-link fw-bolder text-white" href="{{ route('login') }}">{{ __('Login') }}</a>
                             </li>
                             @if (Route::has('register'))
                                 <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                    <a class="nav-link fw-bolder text-white"
+                                        href="{{ route('register') }}">{{ __('Register') }}</a>
                                 </li>
                             @endif
                         @else
                             <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
-                                    data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                <a id="navbarDropdown" class="nav-link  fw-bolder text-white dropdown-toggle" href="#"
+                                    role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false"
+                                    v-pre>
                                     {{ Auth::user()->name }}
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ url('admin') }}">Home</a>
-                                    <a class="dropdown-item" href="{{ url('profile') }}">{{ __('Profile') }}</a>
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                    <a class="dropdown-item text-white" href="{{ url('admin') }}">Home</a>
+                                    <a class="dropdown-item text-white"
+                                        href="{{ url('profile') }}">{{ __('Profile') }}</a>
+                                    <a class="dropdown-item text-white" href="{{ route('logout') }}"
                                         onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
                                         {{ __('Logout') }}
